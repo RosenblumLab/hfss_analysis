f7da8a067ae79e9c3582017f735d10b685ba9aaa
Barkay Guttel <51173082+bguttel@users.noreply.github.com>
2023-05-03 16:30:10 +0300

    Fix typo to prevent failed test

--- tests/test_quantum_formatter.py
+++ tests/test_quantum_formatter.py
@@ -12,15 +12,15 @@ data = {
 mode_to_label = {0: 'hi', 1: 'bye', 5: 'shalom'}
 
 expected = {
-    'hi ND Freq. (Ghz)': 1 * 1e-3,
-    'bye ND Freq. (Ghz)': 2 * 1e-3,
-    'shalom ND Freq. (Ghz)': 3 * 1e-3,
-    'hi - bye Coupling (Mhz)': 1,
-    'hi - shalom Coupling (Mhz)': 3,
-    'bye - shalom Coupling (Mhz)': 10,
-    'hi Anharmonicity (Mhz)': 100,
-    'bye Anharmonicity (Mhz)': 5,
-    'shalom Anharmonicity (Mhz)': 10,
+    'hi ND Freq. (GHz)': 1 * 1e-3,
+    'bye ND Freq. (GHz)': 2 * 1e-3,
+    'shalom ND Freq. (GHz)': 3 * 1e-3,
+    'hi - bye Coupling (MHz)': 1,
+    'hi - shalom Coupling (MHz)': 3,
+    'bye - shalom Coupling (MHz)': 10,
+    'hi Anharmonicity (MHz)': 100,
+    'bye Anharmonicity (MHz)': 5,
+    'shalom Anharmonicity (MHz)': 10,
 }
 
 
